4b6670c5c82624fd60ae682458a1241048a51206
genrommel <rommel.cc.lo@gmail.com>
2023-10-08 21:44:47 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='main'>
+        <section>
+          <h1>My portfolio</h1>g
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
